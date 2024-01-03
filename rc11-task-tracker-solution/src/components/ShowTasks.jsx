72cbad7f7ce940127f516b5bdcf2a566e36b8af3
ceyhunsahin <csahin17072010@gmail.com>
2024-01-03 10:52:52 +0100

    CRUD

--- rc11-task-tracker-solution/src/components/ShowTasks.jsx
+++ rc11-task-tracker-solution/src/components/ShowTasks.jsx
@@ -8,10 +8,41 @@ function ShowTasks({ tasks, setTasks }) {
       )
     );
   };
+  /**
+  |--------------------------------------------------
+  | PUT icin;
+    const toggleData = async (id) => {
+    
+    const {data} = await axios.get(`${baseURL}/${id}`);
+    const updatedTask = {...data, isDone:!data.isDone};
+    await axios.put(`${baseURL}/${id}`, updatedTask);
+    fetchTask();
+
+    PATCH ICIN
+
+    const toggleData = async (id) => {
+    
+    const {data} = await axios.get(`${baseURL}/${id}`);
+    const updatedTask = {...data, isDone:!data.isDone};
+    await axios.patch(`${baseURL}/${id}`, {isDone:!data.isDone});
+    fetchTask();
+
+  |--------------------------------------------------
+  */
 
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
   };
+
+/**
+|--------------------------------------------------
+| const deleteTask = async (id) => {
+    await axios.delete(`${baseURL}/${id}`); // Delete islemi ilgili ID'ye yapilir
+    fetchTask();
+}
+|--------------------------------------------------
+*/
+
   return (
     <div>
       {tasks.map((task) => {
